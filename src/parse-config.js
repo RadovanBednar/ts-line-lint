3ce72d3879ddbea4b58483b3999fddc51204ca3d
RadovanBednar <radovan.bednar@gmail.com>
2018-10-06 23:45:05 +0200

    WIP

--- src/parse-config.js
+++ src/parse-config.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const validate = require('jsonschema').validate;
+const schema = require ('./config.schema.json');
 
 module.exports = function(configFile) {
     let fileContent, configObject;
@@ -6,13 +8,19 @@ module.exports = function(configFile) {
     try {
         fileContent = fs.readFileSync(configFile);
     } catch (e) {
-        throw Error(`Could not open config file "${configFile}" (${e.message})`)
+        throw Error(`Could not open config file "${configFile}" (${e.message})`);
     }
 
     try {
         configObject = JSON.parse(fileContent);
     } catch (e) {
-        throw Error(`Could not parse config file "${configFile}" (${e.message})`)
+        throw Error(`Could not parse config file "${configFile}" (${e.message})`);
+    }
+
+    const validationErrors = validate(configObject, schema).errors;
+
+    if (validationErrors.length > 0) {
+        throw Error(`Invalid config file "${configFile}" (${validationErrors.join(', ')})`);
     }
 
     return {
--- src/parse-config.spec.js
+++ src/parse-config.spec.js
@@ -2,7 +2,7 @@ const fs = require('mock-fs');
 const expect = require('chai').expect;
 const parseConfig = require('./parse-config');
 
-describe('parseConfig function', () => {
+describe.only('parseConfig function', () => {
 
     afterEach(fs.restore);
 
@@ -26,64 +26,28 @@ describe('parseConfig function', () => {
 
     });
 
-    describe('when called with an invalid JSON file', () => {
+    describe('when called with a file that is not valid JSON', () => {
 
         beforeEach(() => {
-            fs({ 'config.json': 'not JSON' });
+            fs({ 'not.json': 'some rubbish' });
         });
 
         it('should throw a "Could not parse config file" error', () => {
-            expect(() => parseConfig('config.json')).to.throw('Could not parse config file');
+            expect(() => parseConfig('not.json')).to.throw('Could not parse config file');
         });
 
     });
 
-    describe.only('when called with a valid JSON file', () => {
+    describe('when called with a JSON file not valid according to the schema', () => {
 
-        describe('and the "indent" property', () => {
-
-            describe('was not specified', () => {
-
-                // beforeEach(() => {
-                //     fs({ 'config.json': '{}' })
-                // });
-
-                it('should equal 4', () => {
-                    fs({ 'config.json': '{}' })
-
-                    expect(parseConfig('config.json').indent).to.equal(4);
-                });
-
+            beforeEach(() => {
+                fs({ 'invalid-config.json': '{ "invalid-property": "invalid-value" }' });
             });
 
-            describe('set to value from 1 to 8', () => {
-
-                it('should equal the given value', () => {
-                    expect(parseConfig('config.json').indent).to.equal(4);
-                });
-
+            it('should throw an "Invalid config file" error', () => {
+                expect(() => parseConfig('invalid-config.json')).to.throw('Invalid config file');
             });
-
-        });
 
     });
-
-    // describe('"indent" property', () => {
-    //
-
-    //
-    //     describe('when it was specified in config file with value from 1 to 8', () => {
-    //
-    //         it('should equal the given value', () => {
-    //             for (let i = 1; i <= 8; i++) {
-    //                 const configFile = `{"indent":${i}}`;
-    //
-    //                 expect(parseConfig(configFile).indent).to.equal(i);
-    //             }
-    //         });
-    //
-    //     });
-    //
-    // });
-
+    
 });
